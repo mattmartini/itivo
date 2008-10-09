9436a2888a4792743d2c699a1ac720c894e5b5ff
yoav.yerushalmi <yoav.yerushalmi@391e5c76-af55-0410-9870-b3ca053975ae>
2008-10-09 20:51:55 +0000

    attempt to cleanly close a file on quit, so that movies will have their moov box set up correctly

--- killProcesses.pl
+++ killProcesses.pl
@@ -1,28 +1,63 @@
 #!/usr/bin/perl
 
+# going through in order trying to kill gently first, and then again hard
+
 $processes = `ps -jAww -o command | grep -e 'iTiVo.app'`;
-
 @lines = split('\n', $processes);
-
 foreach $n (@lines) {
-	if ($n =~ /^[a-z]+\s+(\d+).*\/download/) {
-		`kill -9 $1`;
-	}
 	if ($n =~ /^[a-z]+\s+(\d+).*\/curl/) {
 		`kill $1`;
 	}
+}
+foreach $n (@lines) {
 	if ($n =~ /^[a-z]+\s+(\d+).*\/tivodecode/) {
 		`kill $1`;
 	}
+}
+foreach $n (@lines) {
 	if ($n =~ /^[a-z]+\s+(\d+).*\/mencoder/) {
+		`kill $1`;
+	}
+}
+foreach $n (@lines) {
+	if ($n =~ /^[a-z]+\s+(\d+).*\/download/) {
+		`kill $1`;
+	}
+}
+$processes = `ps -jAww -o command | grep -e 'TiVoDLPipe'`;
+@lines = split('\n', $processes);
+foreach $n (@lines) {
+	if ($n =~ /^[a-z]+\s+(\d+).*\/cat/) {
+		`kill $1`;
+	}
+}
+
+sleep 1;
+$processes = `ps -jAww -o command | grep -e 'iTiVo.app'`;
+@lines = split('\n', $processes);
+foreach $n (@lines) {
+	if ($n =~ /^[a-z]+\s+(\d+).*\/curl/) {
+		`kill -9 $1`;
+	}
+}
+foreach $n (@lines) {
+	if ($n =~ /^[a-z]+\s+(\d+).*\/tivodecode/) {
+		`kill -9 $1`;
+	}
+}
+foreach $n (@lines) {
+	if ($n =~ /^[a-z]+\s+(\d+).*\/mencoder/) {
+		`kill -9 $1`;
+	}
+}
+foreach $n (@lines) {
+	if ($n =~ /^[a-z]+\s+(\d+).*\/download/) {
 		`kill -9 $1`;
 	}
 }
 
 $processes = `ps -jAww -o command | grep -e 'TiVoDLPipe'`;
-
 @lines = split('\n', $processes);
-
 foreach $n (@lines) {
 	if ($n =~ /^[a-z]+\s+(\d+).*\/cat/) {
 		`kill -9 $1`;
