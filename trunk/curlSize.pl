d3908dd59172a74157702fb7020983ca7bf13233
yoav.yerushalmi <yoav.yerushalmi@391e5c76-af55-0410-9870-b3ca053975ae>
2008-11-15 09:50:57 +0000

    fix when source is >> 10G

--- trunk/curlSize.pl
+++ trunk/curlSize.pl
@@ -17,23 +17,29 @@ my @lines = split('\r', $file);
 $line = pop(@lines);
 $prevLine = pop(@lines);
 
-if ($line =~ /^\s+\S+\s+\S+\s+\S+\s+([0-9.]+)([kM]?)\s+\S+\s+\S+\s+[0-9.]+[kM]?\s+\d\s+[0-9\-:]+\s+[0-9\-:]+\s+[0-9\-:]+\s+[0-9.]+[kM]?/) {
+if ($line =~ /^\s*\S+\s+\S+\s+\S+\s+([0-9.]+)([kMG]?)\s+\S+\s+\S+\s+[0-9.]+[kMG]?\s+\d\s+[0-9\-:]+\s+[0-9\-:]+\s+[0-9\-:]+\s+[0-9.]+[kMG]?/) {
 	if ($2 eq "k") {
 		print $1 / 1024;
 	}
 	elsif ($2 eq "M") {
 		print $1;
+	}
+	elsif ($2 eq "G") {
+		print $1 * 1024;
 	}
 	else {
 		print $1 / (1024 * 1024);
 	}
 }
-elsif ($prevLine =~ /^\s+\S+\s+\S+\s+\S+\s+([0-9.]+)([kM]?)\s+\S+\s+\S+\s+[0-9.]+[kM]?\s+\d\s+[0-9\-:]+\s+[0-9\-:]+\s+[0-9\-:]+\s+[0-9.]+[kM]?/) {
+elsif ($prevLine =~ /^\s*\S+\s+\S+\s+\S+\s+([0-9.]+)([kMG]?)\s+\S+\s+\S+\s+[0-9.]+[kMG]?\s+\d\s+[0-9\-:]+\s+[0-9\-:]+\s+[0-9\-:]+\s+[0-9.]+[kMG]?/) {
 	if ($2 eq "k") {
 		print $1 / 1024;
 	}
 	elsif ($2 eq "M") {
 		print $1;
+	}
+	elsif ($2 eq "G") {
+		print $1 * 1024;
 	}
 	else {
 		print $1 / (1024 * 1024);
