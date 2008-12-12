6c52814ae214ce129891f70a43954ea8f3dea556
yoav.yerushalmi <yoav.yerushalmi@391e5c76-af55-0410-9870-b3ca053975ae>
2008-12-12 03:23:33 +0000

    metadata parsing

--- trunk/tivo-decoder.pl
+++ trunk/tivo-decoder.pl
@@ -5,9 +5,14 @@ $MAK   = $ARGV[1];
 
 $file2 =~ s/\%([A-Fa-f0-9]{2})/pack('C', hex($1))/seg;
 
-$shellScript2 = "$file2\Contents/Resources/tivodecode -n -m $MAK -o /tmp/iTiVoDLPipe2-$ENV{'USER'}.mpg /tmp/iTiVoDLPipe-$ENV{'USER'}";
+`mkdir -p /tmp/iTiVoTDC-$ENV{'USER'}`;
+chdir "/tmp/iTiVoTDC-$ENV{'USER'}";
+
+$shellScript2 = "$file2\Contents/Resources/tivodecode -n -D -m $MAK -o /tmp/iTiVoDLPipe2-$ENV{'USER'}.mpg /tmp/iTiVoDLPipe-$ENV{'USER'}";
 
 print "$shellScript2\n";
 system($shellScript2);
 
-`rm /tmp/iTiVoDLPipe-$ENV{'USER'}`;
+`mv chunk-01-0001.xml /tmp/iTiVoDLMeta-$ENV{'USER'}.xml`;
+chdir;
+`rm -rf /tmp/iTiVoDLPipe-$ENV{'USER'} /tmp/iTiVoTDC-$ENV{'USER'}`;
