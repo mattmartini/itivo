0318b70c060428b168cfdf154d68dc12addef729
yoav.yerushalmi <yoav.yerushalmi@391e5c76-af55-0410-9870-b3ca053975ae>
2009-02-06 19:18:08 +0000

    1.7.3b2

--- remove-commercials.pl
+++ remove-commercials.pl
@@ -4,7 +4,11 @@ $encoder   = $ARGV[1];
 
 $file2 =~ s/\%([A-Fa-f0-9]{2})/pack('C', hex($1))/seg;
 
-$Progress = "/tmp/iTiVo-$ENV{'USER'}/iTiVoDL3";
+$TivoDir = "$ENV{'USER'}";
+$TivoDir =~ tr/ :\//_../;
+$TivoDir = "/tmp/iTiVo-$TivoDir";
+
+$Progress = "$TivoDir/iTiVoDL3";
 `dd if=/dev/zero of=$Progress bs=1k count=1`;
-`$file2\Contents/Resources/comskip --ini=$file2\Contents/Resources/comskip.ini /tmp/iTiVo-$ENV{'USER'}/iTiVoDLPipe2.mpg >>$Progress 2>&1`;
-`rm -f /tmp/iTiVo-$ENV{'USER'}/iTiVoDLPipe3.mpg`;
+`$file2\Contents/Resources/comskip --ini=$file2\Contents/Resources/comskip.ini $TivoDir/iTiVoDLPipe2.mpg >>$Progress 2>&1`;
+`rm -f $TivoDir/iTiVoDLPipe3.mpg`;
--- tivo-decoder.pl
+++ tivo-decoder.pl
@@ -5,14 +5,18 @@ $MAK   = $ARGV[1];
 
 $file2 =~ s/\%([A-Fa-f0-9]{2})/pack('C', hex($1))/seg;
 
-`mkdir -p /tmp/iTiVo-$ENV{'USER'}/iTiVoTDC`;
-chdir "/tmp/iTiVo-$ENV{'USER'}/iTiVoTDC";
+$TivoDir = "$ENV{'USER'}";
+$TivoDir =~ tr/ :\//_../;
+$TivoDir = "/tmp/iTiVo-$TivoDir";
 
-$shellScript2 = "$file2\Contents/Resources/tivodecode -n -D -m $MAK -o /tmp/iTiVo-$ENV{'USER'}/iTiVoDLPipe2.mpg /tmp/iTiVo-$ENV{'USER'}/iTiVoDLPipe";
+`mkdir -p $TivoDir/iTiVoTDC`;
+chdir "$TivoDir/iTiVoTDC";
+
+$shellScript2 = "$file2\Contents/Resources/tivodecode -n -D -m $MAK -o $TivoDir/iTiVoDLPipe2.mpg $TivoDir/iTiVoDLPipe";
 
 print "$shellScript2\n";
 system($shellScript2);
 
-`mv chunk-01-0001.xml /tmp/iTiVo-$ENV{'USER'}/iTiVoDLMeta.xml`;
+`mv chunk-01-0001.xml $TivoDir/iTiVoDLMeta.xml`;
 chdir;
-`rm -rf /tmp/iTiVo-$ENV{'USER'}/iTiVoDLPipe /tmp/iTiVo-$ENV{'USER'}/iTiVoTDC`;
+`rm -rf $TivoDir/iTiVoDLPipe $TivoDir/iTiVoTDC`;
